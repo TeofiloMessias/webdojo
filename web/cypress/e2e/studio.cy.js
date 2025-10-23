4737548f7bd857970d2758cbaedcc25e98b00991
Teofilo Messias Filho <teofilo.messias@gmail.com>
2025-10-23 17:51:22 -0300

    Timeout Explicito

--- web/cypress/e2e/studio.cy.js
+++ web/cypress/e2e/studio.cy.js
@@ -2,4 +2,15 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('Deve Logar com Sucesso!', () => {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('http://localhost:3000');    
+    cy.get('#email').type('papito@webdojo.com');    
+    cy.get('#password').type('katana123');
+    cy.contains('button', 'Entrar').click();   
+    cy.get('[data-cy="user-name"]').should('have.text', 'Fernando Papito');
+    /* ==== End Cypress Studio ==== */
+  });
 })
