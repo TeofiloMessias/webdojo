d8037070484a8a7c8a6b77016fc171669da1bf27
TeofiloMessias <teofilo.messias@gmail.com>
2025-12-03 11:42:24 -0300

    Seção 2 - Aula 12 Validando comportamento esperado

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -7,5 +7,15 @@ describe('Login', () => {
     cy.get('#email').type('papito@webdojo.com')
     cy.get('#password').type('katana123')
     cy.contains('button', 'Entrar').click()
+
+    cy.get('[data-cy="user-name"]')
+      .should('be.visible')
+      .and('have.text', 'Fernando Papito')
+    
+      
+    cy.get('[data-cy="welcome-message"]')
+      .should('be.visible')
+      .and('have.text', 'Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
   })
 })
+
