cada2cb57ac60d72ea584b80b304c775bc4c8cfa
TeofiloMessias <teofilo.messias@gmail.com>
2025-12-16 15:46:46 -0300

    Seção 3 - Aula 22 Interagindo com botões Radio

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -7,8 +7,21 @@ describe('Formulario de consultoria', () => {
 
         cy.get('#name').type('Fernando Papito')
         cy.get('#email').type('papito@webdojo.com')
-        cy.get('input[placeholder="(00) 00000-0000"]').type('11954351847')
+        cy.get('input[placeholder="(00) 00000-0000"]')
+            .type('11 99999-1000')
             .should('have.value','(11) 99999-1000')
+
+        cy.get('#consultancyType').select('In Individual')
+
+        cy.contains('label', 'Pessoa Física')
+            .find('input')
+            .check()
+            .should('be.checked')
+
+        cy.contains('label', 'Pessoa Jurídica')
+            .find('input')
+            .should('be.not.checked')
+
+
     })
 })
-
