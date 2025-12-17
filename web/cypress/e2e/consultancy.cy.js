211f3a052eef98ade6307a6ee05f4a04f4aaf771
TeofiloMessias <teofilo.messias@gmail.com>
2025-12-17 15:18:49 -0300

    Seção 3 - Aula 26 Interagindo com Área de Texto

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -45,6 +45,9 @@ describe('Formulario de consultoria', () => {
         cy.get('input[type="file"]')
             .selectFile('./cypress/fixtures/document.pdf', { force: true })
 
+        cy.get('textarea[placeholder="Descreva mais detalhes sobre sua necessidade"]')
+            .type('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+
 
     })
 })
